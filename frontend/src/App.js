457fa02370ddc98f2b3d30e2deeff338f1df2ba5
AnnyHuynh <anny_huynh32@yahoo.com>
2019-06-01 15:33:15 -0700

    updated

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const[fact, setFact] = React.useState('');
+  React.useEffect(() => {
+    fetch('http://localhost:7071/api/cat-facts')
+      .then(raw => raw.json())
+      .then(res => {
+        setFact(res.text);
+      })
+  }, [])
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
